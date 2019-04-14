9d6f7550ab3b3fb21073eb3f624803dd86bd1a81
mamba <1612786059@qq.com>
2019-04-14 17:06:07 +0800

    按钮hover

---
+++ 学习笔记/小demo/按钮hover/script .js
@@ -0,0 +1,9 @@
+b.onclick = function(){
+    b.querySelector('.circle')
+    .classList.add('active')
+}
+b.querySelector('.circle').addEvetListener('transitionend',
+fuvtio(){
+    b.querySelector(".circle")
+    .classList.remove("active")
+})
